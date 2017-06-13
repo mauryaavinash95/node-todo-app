16d3162c4086898a6e2b553a7a2f352ff8478c15
Avinash <mauryaavinash95@gmail.com>
2017-06-13 17:41:50 +0530

    Added patch /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb');
+const _ = require('lodash');
 
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
@@ -42,31 +43,56 @@ app.get("/todos/:id", (request, response) => {
         console.log("Object ID not valid");
         return response.status(404).send("Object ID not valid");
     }
-    Todo.findById( id ).then((todo) => {
-        if(!todo){
+    Todo.findById(id).then((todo) => {
+        if (!todo) {
             return response.status(404).send();
         }
-        response.status(200) .send({todo});
-    }).catch((error) => { 
+        response.status(200).send({ todo });
+    }).catch((error) => {
         response.status(404).send("Couldn't find the object in the database");
     });
 });
 
 app.delete("/todos/:id", (request, response) => {
     var id = request.params.id;
-    if(!ObjectID.isValid(id))
-    {
+    if (!ObjectID.isValid(id)) {
         return response.status(404).send("Object ID not valid");
     }
     Todo.findByIdAndRemove(id).then((todo) => {
-        if(!todo){
+        if (!todo) {
             return response.status(404).send("Object ID not found");
         }
-        response.status(200).send({todo});
+        response.status(200).send({ todo });
     }).catch((error) => {
         response.status(404).send("Some probelem with the object ID");
     });
 });
+
+app.patch("/todos/:id", (request, response) => {
+    var id = request.params.id;
+    var body = _.pick(request.body, ['text', 'completed']);
+    if (!ObjectID.isValid(id)) {
+        return response.status(404).send("Object ID not valid");
+    }
+
+    if (_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    }
+    else {
+        body.completed = false;
+        body.completeAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
+        if (!todo) {
+            return response.status(404).send("Todo not found");
+        }
+        response.send({ todo })
+    }).catch((error) => {
+        response.status(404).send();
+    });
+
+})
 
 app.post('/user', (request, response) => {
     var user = new User({
@@ -90,7 +116,7 @@ app.get('/todos', (request, response) => {
 });
 
 app.listen(port, () => {
-    console.log("Started on port "+port);
+    console.log("Started on port " + port);
 });
 
 module.exports = { app };
